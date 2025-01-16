7571cb906538fbc8028c528a8ef638c0b510f342
Gustavo de Freitas Fidelis <gustavo.fidelis@edu.pucrs.br>
2025-01-15 21:01:00 -0300

    Adicao das classes 'Administracao' e 'Veiculo', algumas modificacoes feitas na classe 'Veiculo'

---
+++ src/dados/Veiculo.java
@@ -0,0 +1,17 @@
+package dados;
+
+public class Veiculo {
+    private String placa;
+    private String marca;
+    private String modelo;
+    private String cor;
+    private int ano;
+    private String tipo;
+    private double quilometragem;
+    private String historico;
+
+
+    public Veiculo(String placa, String marca, String modelo, String cor, int ano, String tipo, double quilometragem){
+
+    }
+}
